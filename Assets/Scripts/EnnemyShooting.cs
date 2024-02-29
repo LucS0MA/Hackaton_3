a60c40c33b4f38b4d1302cd7a56602990f180d31
Hamidou <150685145+HamIIdou@users.noreply.github.com>
2024-02-29 10:11:44 +0100

    Add the bullet and lost health when he get touch

---
+++ Assets/Scripts/EnnemyShooting.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class EnnemyShooting : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletPos;
+    private float timer;
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float distance = Vector2.Distance(transform.position, player.transform.position);
+        Debug.Log(distance);
+
+        if(distance < 10)
+        {
+            timer += Time.deltaTime;
+            if (timer > 2.1)
+            {
+                timer = 0;
+                shoot();
+            }
+        }
+
+      
+    }
+
+    void shoot()
+    {
+        Instantiate(bullet, bulletPos.position, Quaternion.identity);
+    }
+}
